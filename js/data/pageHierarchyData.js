42ef9340686ba9f4fb146931b2bdcb0b5bcee859
Sybren Keizer <92291687+sybrenkeizer@users.noreply.github.com>
2025-02-20 19:03:15 +0100

    Update pageHierarchyData.js

--- js/data/pageHierarchyData.js
+++ js/data/pageHierarchyData.js
@@ -1,10 +1,9 @@
 import { storeInLS, getFromLS } from "../utilities/localStorage.js";
 
 export default async function storePageHierarchyDataInLS() {
-    const url = '../json/pageHierarchy.json';
     return new Promise((resolve, reject) => {
         if (!getFromLS('pageHierarchyData')) {
-            fetch(url)
+            fetch('../json/pageHierarchy.json')
                 .then(response => response.json())
                 .then(json => {
                     storeInLS('pageHierarchyData', json);
