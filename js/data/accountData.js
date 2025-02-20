654aa2442c0421ffa147f0b6cccf7ce473c696d8
Sybren Keizer <92291687+sybrenkeizer@users.noreply.github.com>
2025-02-20 19:10:08 +0100

    Update accountData.js

--- js/data/accountData.js
+++ js/data/accountData.js
@@ -3,7 +3,7 @@ import { storeInLS, getFromLS } from "../utilities/localStorage.js";
 export default async function storeAccountDataInLS() {
     return new Promise((resolve, reject) => {
         if (!getFromLS('accountData')) {
-            fetch('./json/accounts.json')
+            fetch('json/accounts.json')
                 .then(response => response.json())
                 .then(json => {
                     storeInLS('accountData', json);
